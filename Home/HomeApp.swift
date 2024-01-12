a66afd3c2a6bf00f207a0e34fbdafb0b7d5b87a5
Hiro Tanaka <HTanaka@lcs.on.ca>
2024-01-12 08:37:02 -0500

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by 田中宏貴 on 2024-01-12.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
